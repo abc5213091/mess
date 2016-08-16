21f8a71838a4509a537e947e8aed25367a29057c
renzhe1983 <275280443@qq.com>
2016-08-16 10:05:31 +0800

    api2.0

--- API/routes/article.js
+++ API/routes/article.js
@@ -5,11 +5,7 @@ var Article = require('../DB/controllers/Article')
 
 router.get('/', Article.list);
 router.get('/:id', Article.getListByID);
-router.get('/save', Article.save);
-router.get('/del', Article.del);
-// router.get('/:id', function(req,res){
-
-// 	res.json({ name: req.params.id , id: req.query.id})
-// });
+router.post('/save', Article.save);
+router.delete('/del', Article.del);
 
 module.exports = router;
